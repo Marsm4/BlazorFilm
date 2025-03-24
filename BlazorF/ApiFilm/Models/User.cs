f736fb12fc3b8b2fd8c47e0e9d405b4c3a27a711
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2025-03-24 19:32:24 +0300

    Добавьте файлы проекта.

---
+++ BlazorF/ApiFilm/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorF.ApiFilm.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
